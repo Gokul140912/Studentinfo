f72369536eff86fea1b4b015e2cb577f5696e597
Gokul140912 <155716653+Gokul140912@users.noreply.github.com>
2024-01-05 16:05:51 +0530

    Update Vercel.py

--- Vercel.py
+++ Vercel.py
@@ -5,7 +5,7 @@ import pandas as pd
 import plotly.express as px
 
 # Load data from an Excel file with a specific sheet
-excel_file_path = 'Text file.xlsx'
+excel_file_path = 'https://github.com/Gokul140912/Tst/blob/main/Test%20file.xlsx'
 sheet_name = 'Sheet1'  # Replace with the actual sheet name
 
 try:
